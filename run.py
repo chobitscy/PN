59a83909e52a3605b533c1bbe238d5bd30f689b8
pn <duhongyu@szlx.co>
2022-04-20 10:22:46 +0800

    feat: 自定义排序

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ import logging
 import sys
 import time
 
+import flask_limiter
 from flask import Flask, jsonify, abort, make_response, request
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
@@ -23,12 +24,13 @@ app.config['JSON_AS_ASCII'] = False
 args = sys.argv
 if len(args) <= 1:
     raise ValueError('参数不完整')
-db_host, db_port, db_user, db_password, db_name, host = args[1], args[2], args[3], args[4], args[5], args[6]
+db_host, db_port, db_user, db_password, db_name, host, env = args[1], args[2], args[3], args[4], args[5], args[6], args[
+    7]
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://%s:%s@%s:%s/%s" % (db_user, db_password, db_host, db_port, db_name)
 db = SQLAlchemy(app)
 
 # 限速
-limiter = Limiter(app=app, key_func=get_remote_address, default_limits=["20/minute"])
+limiter = Limiter(app=app, key_func=get_remote_address, default_limits=["2/minute"])
 
 
 class BaseMixin:
@@ -77,6 +79,10 @@ class VideoSchema(BaseSchema):
 # 全局异常处理
 @app.errorhandler(Exception)
 def handle_exception(e):
+    if isinstance(e, flask_limiter.errors.RateLimitExceeded):
+        return jsonify({
+            'message': 'Too Many Requests'
+        }), 429
     logging.exception(e)
     return jsonify({
         'message': 'system error'
@@ -91,8 +97,8 @@ def request_handle():
     if timestamp == 0 or sign is None:
         abort(make_response(jsonify({'message': 'Illegal request'}), 400))
     # 当客户端和服务器的时间相差 20 秒，客户端错误
-    if int(round(time.time() * 1000)) - int(timestamp) > 20 * 1000:
-        abort(make_response(jsonify({'message': 'Illegal request'}), 400))
+    if env != 'dev' and int(round(time.time() * 1000)) - int(timestamp) > 20 * 1000:
+        abort(make_response(jsonify({'message': 'timeout'}), 400))
     value = str(hashlib.md5(str('%.2f' % float(timestamp / len(request.path))).encode('utf-8')).hexdigest()).upper()
     if value is None or value != sign:
         abort(make_response(jsonify({'message': 'Illegal request'}), 400))
@@ -100,16 +106,15 @@ def request_handle():
 
 @app.route('/popular/<day>', methods=['GET'])
 def get_list(day):
-    page = request.args.get('page', 1, int)
-    pages = request.args.get('pages', 10, int)
+    page, pages, sort = parameter_handler(Video, '-rate')
     try:
         day = int(day)
     except ValueError:
-        abort(make_response(jsonify({'message': 'parameter error'}), 400))
+        error('parameter error', 400)
     now = datetime.datetime.now()
     pagination = Video.query \
         .filter(Video.pub_date.between(now - datetime.timedelta(days=day), now)) \
-        .order_by(Video.rate.desc()) \
+        .order_by(sort) \
         .paginate(page, per_page=pages, error_out=False)
     return jsonify({
         'data': {
@@ -119,6 +124,26 @@ def get_list(day):
             'total': pagination.total
         }
     })
+
+
+def error(message, code):
+    abort(make_response(jsonify({'message': message}), code))
+
+
+def parameter_handler(o: db.Model, default_sort):
+    page = request.args.get('page', 1, int)
+    pages = request.args.get('pages', 10, int)
+    sort = request.args.get('sort_by', default_sort, str)
+    sort_way = sort[0]
+    sort_by = sort[1:]
+    attribute = None
+    if hasattr(o, sort_by):
+        attribute = getattr(o, sort_by)
+        if sort_way == '-':
+            attribute = attribute.desc()
+    else:
+        error('parameter error', 400)
+    return page, pages, attribute
 
 
 if __name__ == '__main__':

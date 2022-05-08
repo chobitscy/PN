8705e5c3b46d49fc53505bfe9128ea4571475f81
pn <null>
2022-05-08 14:17:54 +0800

    fix(run): register or login not auth

--- run.py
+++ run.py
@@ -36,17 +36,21 @@ def request_handle():
         return
     timestamp = request.headers.get('timestamp', 0, int)
     sign = request.headers.get('sign', None, str)
+    path = request.path
     if timestamp == 0 or sign is None:
         abort(make_response(jsonify({'message': 'illegal request'}), 400))
     # 当客户端和服务器的时间相差 20 秒，客户端错误
     if env != 'dev' and int(round(time.time() * 1000)) - int(timestamp) > 20 * 1000:
         abort(make_response(jsonify({'message': 'timeout'}), 400))
     decimal.getcontext().rounding = "ROUND_HALF_UP"
-    res = decimal.Decimal(str(float(timestamp / len(request.path)))).quantize(decimal.Decimal("0.00"))
+    res = decimal.Decimal(str(float(timestamp / len(path)))).quantize(decimal.Decimal("0.00"))
     value = str(
         hashlib.md5(str(res).encode('utf-8')).hexdigest()).upper()
     if value is None or value != sign:
         abort(make_response(jsonify({'message': 'sign error'}), 400))
+    # register or login not auth
+    if path == '/user/register' or path == '/user/login':
+        return
     if 'Authorization' not in request.headers:
         error('auth error', 401)
     token = request.headers["Authorization"]
